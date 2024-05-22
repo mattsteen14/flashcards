b1c05789dfaf08972e8631144e0702b4126bcb01
mattsteen14 <mattsteen14@me.com>
2024-05-22 11:48:03 +0100

    selectTopics in Topics

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
+import topicsReducer from '../features/topics/topicsSlice.js';
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsReducer
   },
 });
--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
 // import selector
+import { useSelector } from "react-redux";
+import { selectTopics } from "./topicsSlice";
 
 export default function Topics() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
-
+  const topics = useSelector(selectTopics); // replace this with a call to your selector to select all the topics in state
+  console.log(topics);
   return (
     <section className="center">
       <h1>Topics</h1>
