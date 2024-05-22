f563ddcfc249a00adc8b094ceef3be059f4a0bf4
mattsteen14 <mattsteen14@me.com>
2024-05-22 12:45:34 +0100

    quizzesSlice

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+export const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: {
+        quizzes: {}
+    },
+    reducers: {
+        addQuiz: (state, action) => {
+            const id = uuidv4();
+            const { name, topicId } = action.payload;
+            state.quizzes[id] = {
+                id,
+                name,
+                topicId,
+                cardIds: []
+            };
+        }
+    }
+})
+
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
