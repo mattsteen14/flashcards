58b2318e16491684411175cffb7f3f3e792daa47
mattsteen14 <mattsteen14@me.com>
2024-05-23 10:59:18 +0100

    cardsReducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import reducers
 import topicsReducer from '../features/topics/topicsSlice.js';
 import quizzesReducer from '../features/quizzes/quizzesSlice.js';
+import cardsReducer from '../features/cards/cardsSlice.js'
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: quizzesReducer
+    quizzes: quizzesReducer,
+    cards: cardsReducer
   },
 });
