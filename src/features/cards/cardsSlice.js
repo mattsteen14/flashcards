31992516e302448f4cbd4854ef1bc22fe9d7b419
mattsteen14 <mattsteen14@me.com>
2024-05-23 10:57:12 +0100

    cardsSlice

---
+++ src/features/cards/cardsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: {
+        cards: {}
+    },
+    reducers: {
+        addCard: (state, action) => {
+            const { id, front, back } = action.payload;
+            state.cards[id] = {
+                id,
+                front,
+                back
+            };
+        }
+    }
+})
+
+export const selectCardById = (id) => (state) => state.cards.cards[id];
+// export const selectCardById = (id) => (state) => state.items.find(item.id === id);
+// export const selectCards = (state) => state.cards.cards;
+export const { addCard } = cardsSlice.actions;
+export default cardsSlice.reducer;
